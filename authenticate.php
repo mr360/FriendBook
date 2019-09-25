231be0c701903cc0d2fc96d4021d6f9186f3a477
mr360 <qd0097@gmail.com>
2019-09-25 16:55:38 +1000

    added boilerplate code for authenticate

--- authenticate.php
+++ authenticate.php
@@ -6,3 +6,35 @@ Authenticate
     SetSession 
     KillSession
 */
+
+<?php
+class Authenticate
+{
+    public function Login(User $aUser)
+    {
+        // if login success call SetSession()
+        // return true/false
+    }
+
+    public function Register(User $aUser)
+    {
+        // return true/false
+    }
+
+    public function Logout()
+    {
+        // return true/false
+    }
+
+    private function SetSession()
+    {
+        // no return
+    }
+
+    private function KillSession()
+    {
+        // no return
+    }
+}
+
+?>
